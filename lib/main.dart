d5b1e5ef8c87ec43b95e2df953af7698c539fa11
gramjos <graham.joss@gmail.com>
2023-12-24 14:56:32 -0600

    small nothing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
 import 'app.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void main() => runApp(const App());
+void main() {
+  runApp(const App());
+}
